7379434f291348c48114e79da9a6060679e5992e
Steven Cormack <stevec@Stevens-MBP.lan>
2021-04-23 13:07:13 +0100

    basic structure

---
+++ db/nhl_league.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS team_results;
+DROP TABLE IF EXISTS teams;
+DROP TABLE IF EXISTS matches;
+DROP TABLE IF EXISTS players;
+-- DROP TABLE IF EXISTS leagues;
+
+CREATE TABLE teams (
+
+);
+
+CREATE TABLE players (
+
+);
+
+CREATE TABLE matches (
+
+);
+
+-- CREATE TABLE leagues (
+
+-- );
+
+CREATE TABLE team_results (
+
+);
+
