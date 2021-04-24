593606bb11d6bef1e4918b4ba01e36e730927de5
Steven Cormack <stevec@Stevens-MBP.lan>
2021-04-24 19:59:52 +0100

    team repository complete

--- repositories/team_repository.py
+++ repositories/team_repository.py
@@ -12,6 +12,61 @@ def save(team):
     team.id = id
     return team
 
+def select_all():
+    teams = []
+
+    sql = "SELECT * FROM teams"
+    results = run_sql(sql)
+
+    for row in results:
+        team = Team(row['name'], row['coach'], row['stadium'], row['city'])
+        teams.append(team)
+    return teams
+
+def select(id):
+    team = None:
+    sql = "SELECT * FROM teams WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        team = Team(result['name'], result['coach'], result['stadium'], result['city'])
+    return team
+
+def players(team):
+    players = []
+
+    sql = "SELECT * FROM players WHERE team_id = %s"
+    values = [team.id]
+    results = run_sql(sql, values)
+
+    for row in results:
+        player = Player(row['first_name'], row['last_name'], row['age'], row['team_id'], row['position'], row['points'])
+        players.append(player)
+    return players
+
+def matches(team):
+    matches = []
+
+    sql = "SELECT * FROM matches WHERE home_team_id = %s OR away_team_id = %s"
+    values = [team.id, team.id]
+    results = run_sql(sql, values)
+
+    for row in results:
+        match = Match(row['date'], row['home_team_id'], row['home_team_score'], row['away_team_id'], row['away_team_score'], row['winner'])
+        matches.append(match)
+    return matches
+
+def update(id):
+    sql = "UPDATE teams SET (name, coach, stadium, city) = (%s, %s, %s, %s) WHERE id = %s"
+    values = [team.name, team.coach, team.stadium, team.city, team.id]
+    run_sql(sql, values)
+
+def delete(id):
+    sql = "DELETE FROM teams WHERE id = %s"
+    values[id]
+    run_sql(sql, values)
+
 def delete_all():
     sql = "DELETE FROM teams"
     run_sql(sql)
