837ef9ad251fa7155277a4c9e65409a23bcf752d
Steven Cormack <stevec@Stevens-MBP.lan>
2021-04-28 17:59:38 +0100

    dynamic sidebar

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 
+import repositories.team_repository as team_repository
 from controllers.teams_controller import teams_blueprint
 from controllers.players_controller import players_blueprint
 from controllers.matches_controller import matches_blueprint
@@ -13,7 +14,8 @@ app.register_blueprint(matches_blueprint)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    teams = team_repository.select_all()
+    return render_template('index.html', teams=teams)
 
 if __name__ == '__main__':
     app.run(debug=True)
